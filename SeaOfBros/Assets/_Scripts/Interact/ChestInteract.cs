64e8c293c3a265b0e5c1341f5da16f17aaf9c495
Brad Farris <bfarris1@mail.csuchico.edu>
2024-11-05 10:29:36 -0800

    Water Assets Imported + Tests

--- SeaOfBros/Assets/_Scripts/Interact/ChestInteract.cs
+++ SeaOfBros/Assets/_Scripts/Interact/ChestInteract.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChestInteract : MonoBehaviour//, IInteractable
+public class ChestInteract : MonoBehaviour, IInteractable
 {
     
+    public void Interact(GameObject interactor)
+    {
 
+
+
+
+
+    }
 
 
 
