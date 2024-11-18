36508dbb7e748f81a5b6598dc89d4d8792d630c5
Brad Farris <bfarris1@mail.csuchico.edu>
2024-11-18 14:37:12 -0800

    Interaction progress
    - Player is able to pick up the chest object, but there needs to be some changes for the transform position.
    - Chest does not always stay at the right rotation/position and will get knocked out of player's hands if it collides with anything
    - Setup basic crosshair to proc when you are able to interact with something
    - Still have to work on actual harpoon interactions

--- SeaOfBros/Assets/_Scripts/Interact/ChestInteract.cs
+++ SeaOfBros/Assets/_Scripts/Interact/ChestInteract.cs
@@ -1,17 +1,117 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityStandardAssets.Utility;
+using UnityEngine.InputSystem;
 
-public class ChestInteract : MonoBehaviour, IInteractable
+public class ChestInteract : MonoBehaviour//, IInteractable
 {
-    [SerializeField] private string _prompt;
+    [SerializeField] private GameObject crosshair;
+    [SerializeField] private Transform objTransform, playerTransform;
+    [SerializeField] private Rigidbody chestRigidBody;
+    private bool interactable, objPickedUp;
     
-    public string InteractionPrompt => _prompt;
 
-    public bool Interact(PlayerInteractor interactor)
+
+
+    private void OnTriggerStay(Collider other)
     {
-        Debug.Log("Chest Picked Up!");
-        return true;
+        if (other.CompareTag("MainCamera"))
+        {
+            crosshair.SetActive(true);
+            interactable = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            if (objPickedUp == true)
+            {
+                objTransform.parent = null;
+                chestRigidBody.useGravity = true;
+                interactable = false;
+                objPickedUp = false;
+            }
+            else if (objPickedUp == false)
+            {
+                crosshair.SetActive(false);
+                interactable = false;
+            }
+            Debug.Log("OnTriggerExit Proccd");
+        }
+    }
+
+
+
+    private void Start()
+    {
+        chestRigidBody = GetComponent<Rigidbody>();
+        objTransform = GetComponent<Transform>();
+
+        if (chestRigidBody != null )
+        {
+            Debug.Log("chestRigidBody loaded");
+        }
+        if (objTransform != null )
+        {
+            Debug.Log("objTransform = " + objTransform);
+        }
+    }
+
+
+    private void Update()
+    {
+        if (interactable == true)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                objTransform.parent = playerTransform;
+                crosshair.SetActive(false);
+                chestRigidBody.useGravity = false;
+                objPickedUp = true;
+                Debug.Log("Interact triggered");
+            }
+            if (objPickedUp == true && Input.GetButtonDown("Drop"))
+            {
+                objTransform.parent = null;
+                chestRigidBody.useGravity = true;
+                objPickedUp = false;
+                Debug.Log("Drop triggered");
+            }
+        }
+
+
+
+
 
     }
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+/*
+[SerializeField] private string _prompt;
+
+public string InteractionPrompt => _prompt;
+
+public bool Interact(PlayerInteractor interactor)
+{
+    Debug.Log("Chest Picked Up!");
+    return true;
+
+}
+*/
--- SeaOfBros/Assets/_Scripts/Interact/Interact.cs
+++ SeaOfBros/Assets/_Scripts/Interact/Interact.cs
@@ -4,16 +4,54 @@ using UnityEngine;
 
 /*
  * Author: [Brad Farris]
- * Updated: 10.30.24
+ * Updated: 11.18.24
  * Purpose: Interaction
  */
 
 
+public class Interact : MonoBehaviour
+{
+    public GameObject crosshair;
+    public Transform objTransform, playerTransform;
+    public bool interactable;
 
-    // GameObject should be what is doing the interaction
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            crosshair.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            crosshair.SetActive(false);
+
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+/* GameObject should be what is doing the interaction
 
     public interface IInteractable
     {
         public string InteractionPrompt {  get; }    
         public bool Interact(PlayerInteractor interactor);
     }
+
+
+*/
