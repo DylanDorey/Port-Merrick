4008024ca107df4ac28ef7a814d8e52de1dcd1ad
Brad Farris <bfarris1@mail.csuchico.edu>
2024-12-12 12:03:30 -0800

    explsion prefab/script

---
+++ SeaOfBros/Assets/_Scripts/Explosion.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float ExplosiveFuse = 10;
+    public float Explosiveforce = 10;
+    public float ExplosiveRadius = 10;
+
+
+    private void Start()
+    {
+        Invoke("ExplosionDamage", ExplosiveFuse);
+    }
+    void ExplosionDamage()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, ExplosiveRadius);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.GetComponent<Rigidbody>() != null)
+            {
+                Debug.Log("hit");
+                hitCollider.GetComponent<Rigidbody>().AddExplosionForce(Explosiveforce, this.transform.position, ExplosiveRadius);
+            }
+        }
+    }
+}
